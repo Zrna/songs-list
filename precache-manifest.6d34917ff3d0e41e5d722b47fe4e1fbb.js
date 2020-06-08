f4717da4315cb30b90c96b3ffffc2c4f73a24ae3
Zrna <lukazrnic5@gmail.com>
2020-06-08 12:20:40 +0200

    Updates

---
+++ precache-manifest.6d34917ff3d0e41e5d722b47fe4e1fbb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee190a2ee8612c21124bfa99ef6592d6",
+    "url": "/songs-list/index.html"
+  },
+  {
+    "revision": "5f14a75341c655cfd3f3",
+    "url": "/songs-list/static/css/2.257f4af1.chunk.css"
+  },
+  {
+    "revision": "f63e07bc53eb90e22768",
+    "url": "/songs-list/static/css/main.082c5ea5.chunk.css"
+  },
+  {
+    "revision": "5f14a75341c655cfd3f3",
+    "url": "/songs-list/static/js/2.cbd4ca25.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/songs-list/static/js/2.cbd4ca25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63e07bc53eb90e22768",
+    "url": "/songs-list/static/js/main.484fe6c3.chunk.js"
+  },
+  {
+    "revision": "a7c23dec3a93780a0bd5",
+    "url": "/songs-list/static/js/runtime-main.0fd1109a.js"
+  },
+  {
+    "revision": "8b9d35f0b4a9037bb173023d6b1e95f4",
+    "url": "/songs-list/static/media/error.8b9d35f0.svg"
+  },
+  {
+    "revision": "6d13e4792cd7381742c3b72f777e69b1",
+    "url": "/songs-list/static/media/pen.6d13e479.svg"
+  },
+  {
+    "revision": "a9966d7493439682a02f385cc116773b",
+    "url": "/songs-list/static/media/trash.a9966d74.svg"
+  }
+]);
